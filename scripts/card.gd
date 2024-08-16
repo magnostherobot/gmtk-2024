0b395a6af293d0f6f2ada082caba77dfe51c457b
Maggie Harley <mag@magnostherobot.co.uk>
2024-08-16 22:11:10 +0100

    Show card rank

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,15 +1,12 @@
 @tool
 extends Node2D
 
-@export var count: int = 0
 @export var rank: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	$Label.text = str(count)
-	$Card.rank = rank
+func _process(delta: float) -> void:
+	$Sprite2D.frame =  rank - 1
