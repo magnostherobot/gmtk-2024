c628a4b7a5ebc6d0b15b9edf0c59fae1dbdfc9d6
Maggie Harley <mag@magnostherobot.co.uk>
2024-08-17 15:19:34 +0100

    Add throw button logic

---
+++ scenes/throw-button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+@export var pile: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	pile.play_cards(2, 1982)
