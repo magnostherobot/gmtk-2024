314efb5aa4fb8ede404dfad5211ab8cdc444b934
Maggie Harley <mag@magnostherobot.co.uk>
2024-08-17 15:15:11 +0100

    Add throw button
    For debugging.

--- scripts/set_pile.gd
+++ scripts/set_pile.gd
@@ -2,15 +2,21 @@ extends CPUParticles2D
 
 
 func play_cards(rank: int, count: int) -> void:
+	clear()
 	self.amount = count
+	self.lifetime = 1
 	# TODO: update texture in use
 	self.emitting = true
 	self.speed_scale = 1
 	$Timer.start()
 
+func clear() -> void:
+	self.restart()
+	self.emitting = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play_cards(1, 30)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
